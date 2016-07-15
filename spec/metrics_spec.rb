4398a333acd05bd60cd21c41982699941eb3300f
Stefan Dang <stefan.dang@studium.uni-hamburg.de>
2016-07-15 17:57:33 +0200

    added intermediate file

--- spec/metrics_spec.rb
+++ spec/metrics_spec.rb
@@ -9,10 +9,10 @@ describe Ribopip::Metrics::GtSeqstat do
       './spec/testdata/statistics/SRR315601_625.fastq.gt_seqstat'
     )
     # only the first 6 lines are relevant
-    `head -n 6 ./spec/testdata/statistics/SRR315601_625.fastq.gt_seqstat > ./spec/testdata/statistics/SRR315601_625.fastq.gt_seqstat`
+    `head -n 6 ./spec/testdata/statistics/SRR315601_625.fastq.gt_seqstat > ./spec/testdata/statistics/n6.gt_seqstat`
     expect(
       FileUtils.compare_file(
-        './spec/testdata/statistics/SRR315601_625.fastq.gt_seqstat',
+        './spec/testdata/statistics/n6.gt_seqstat',
         './spec/testdata/statistics/expected.gt_seqstat'
       )
     ).to be_truthy
