a55fae631f72c6f4d038cae6bb280c1ace83cab5
Stefan Dang <stefan.dang@studium.uni-hamburg.de>
2016-07-15 17:46:14 +0200

    modified .gt_seqstat test to account for dynamic output

--- spec/metrics_spec.rb
+++ spec/metrics_spec.rb
@@ -4,11 +4,17 @@ require 'ribopip/metrics'
 
 describe Ribopip::Metrics::GtSeqstat do
   it '.compute' do
-    Ribopip::Metrics::GtSeqstat.compute('./spec/testdata/SRR315601_625.fastq',
-                                       './spec/testdata/statistics/SRR315601_625.fastq.gt_seqstat')
+    Ribopip::Metrics::GtSeqstat.compute(
+      './spec/testdata/SRR315601_625.fastq',
+      './spec/testdata/statistics/SRR315601_625.fastq.gt_seqstat'
+    )
+    # only the first 6 lines are relevant
+    `head -n 6 ./spec/testdata/SRR315601_625.fastq > ./spec/testdata/SRR315601_625.fastq`
     expect(
-      FileUtils.compare_file('./spec/testdata/statistics/SRR315601_625.fastq.gt_seqstat',
-                             './spec/testdata/statistics/expected.gt_seqstat')
+      FileUtils.compare_file(
+        './spec/testdata/statistics/SRR315601_625.fastq.gt_seqstat',
+        './spec/testdata/statistics/expected.gt_seqstat'
+      )
     ).to be_truthy
   end
 end
